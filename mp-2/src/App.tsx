e1df4436c447775b5f23d43ba4bc2bc01e2c0dc6
jasons1142 <jasons11@bu.edu>
2025-02-19 16:17:44 -0500

    Updates to mp-2

---
+++ mp-2/src/App.tsx
@@ -0,0 +1,14 @@
+import Dogs from "./components/Dogs.tsx";
+import styled from "styled-components";
+import { useEffect, useState } from 'react'
+import { Joke } from './interfaces/Dogs.ts'
+
+
+export default function App() {
+
+  const [data, setData] = useState<Joke[]>
+    
+  
+}
+
+
