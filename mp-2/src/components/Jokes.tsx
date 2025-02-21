0e56ac0ecdea1eaa6f5b866936f4a48f2e9f1485
jasons1142 <jasons11@bu.edu>
2025-02-21 11:32:42 -0500

    Update to Jokes.tsx

--- mp-2/src/components/Jokes.tsx
+++ mp-2/src/components/Jokes.tsx
@@ -1,10 +1,36 @@
-import {useState} from "react"
+import {useEffect, useState} from "react"
 import {styled} from "styled-components";
 import {Joke} from "../interfaces/Jokes.ts";
 
+const JokesPreviewDiv = styled.div`
+    margin: 3rem;
+    padding: 1rem;
+    width: 20rem;
+    background-color: blue;
+`;
+
+const JokesPreview = ({joke}: {joke: Joke}) => {
+    return (
+        <JokesPreviewDiv>
+            <h3>{joke.type}</h3>
+            <p>{joke.setup}</p>
+            <p>{joke.punchline}</p>
+        </JokesPreviewDiv>
+    )
+}
 
 export default function JokeContent() {
     const [joke, setJoke] = useState<string | null>(null)
+    const [jokes, setJokes] = useState<Joke[]>([])
+
+    useEffect(() => {
+        async function getJoke() {
+            const res = await fetch(`https://official-joke-api.appspot.com/random_joke=${joke}`);
+            const data = await res.json();
+            setJokes(data.data);
+        }
+        getJoke();
+    })
 
     return (
         <div>
