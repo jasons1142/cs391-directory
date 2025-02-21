ebd94d2a892d682fd4c3b85d60b946352dbae800
jasons1142 <jasons11@bu.edu>
2025-02-21 10:40:31 -0500

    Changed filename

--- mp-2/src/components/Dogs.tsx
+++ mp-2/src/components/Dogs.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import {styled} from "styled-components";
 import {Joke} from "../interfaces/Dogs.ts";
 
@@ -7,33 +8,3 @@ const AllCharsDiv=styled.div`
     justify-content: space-evenly;
     background-color: bisque;
 `;
-
-const SingleJokeDiv=styled.div<{status: string}>`
-    display: flex;
-    flex-direction: column;   
-    justify-content: center;
-    max-width: 30%;
-    padding: 2%;
-    margin: 1%;
-    background-color: ${(props)=>(props.status === "Alive" ? 'darkorange' : 'black')};
-    color: ${(props) => (props.status !== "Alive" ? 'white' : 'black')};
-    border: 3px darkred solid;
-    font: italic small-caps bold calc(2px + 1vw) Papyrus, fantasy;
-    text-align: center;
-`;
-
-export default function Dogs(props : { data:Joke[] } ){
-    return (
-        <AllCharsDiv >
-            {
-                props.data.map((char: Joke) =>
-                    <SingleJokeDiv key={char.id} status={char.type}>
-                        <h1>{char.setup}</h1>
-                        <p>{char.punchline !== "Human" ? "(Not Human)" : ""}</p>
-                        <img src={char.punchline} alt={`image of ${char.punchline}`} />
-                    </SingleJokeDiv>
-                )
-            }
-        </AllCharsDiv>
-    );
-}
